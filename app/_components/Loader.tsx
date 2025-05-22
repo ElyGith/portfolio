3f757b788e9fdb9e3abfdee534e943e28e647f3f
Ely <ely.wandeu@tu.univ-lyon1.fr>
2025-05-22 13:02:08 +0200

    loader

---
+++ app/_components/Loader.tsx
@@ -0,0 +1,12 @@
+
+
+
+export const Loader = ()=>{
+
+    return (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-white">
+            <div className="text-2xl">Chargement...</div>
+        </div>
+      );
+
+}
